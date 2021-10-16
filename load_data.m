5c62e409d0c567e2db3061bb0fed81de0984506a
lauracarlton <91345605+lauracarlton@users.noreply.github.com>
2021-10-16 18:54:27 -0400

    Add files via upload

---
+++ load_data.m
@@ -0,0 +1,136 @@
+%% NOTES BEFORE RUNNING
+% Make sure current folder is DropBox 
+% adds all MATLAB tools
+
+addpath('MATLAB tools/jsonlab-2.0/')
+addpath('MATLAB tools/nlid_tools/')
+addpath('MATLAB tools/utility_tools/')
+%%
+clc
+clear all
+tic 
+baseDir = 'trials_data_json/ANNE_data_trial';
+descrip_path ='intermittentBreathing_voluntary';
+ntrial = '002';
+
+filename = string([baseDir ntrial '_' descrip_path '.json']);
+%savepath = ['trials_data_json/Export/figures_v1/' ntrial];
+if  0 %~exist(savepath, 'file')
+    %mkdir(savepath)
+end
+
+description = "voluntary intermittent breathing";
+raw_data = loadjson(filename);
+
+
+%% go through each cell in the raw data and assign it to a structure
+package_gap_counter =1;
+duplicate_data_counter = 1;
+for a = 1:length(raw_data)
+    
+    cell = raw_data{a};
+    datatype = cell.dataType;
+    if datatype == "Health"
+        continue
+    end
+    sensor = cell.sensor_name;
+    try
+        all_data.(sensor).(datatype)(end+1) = cell;
+    catch
+        all_data.(sensor).(datatype) = cell;
+    end
+    
+    if length(all_data.(sensor).(datatype)) >= 2
+ 
+        time_diff = zeros(length(cell.timestamp)-1,1);
+        for j = 2:length(cell.timestamp)
+            time_diff(j-1) = cell.timestamp(j) - cell.timestamp(j-1);
+        end
+        Ts = mean(time_diff);
+        
+        %CHECKING FOR PACKAGE SIZED GAPS
+        %STILL NEED TO COMPARE TO HISTOGRAMS OF TIME GAPS, CHANGE COUNTER,
+        %...
+        if cell.timestamp >1.5*Ts*length(cell.timestamp)+all_data.(sensor).(datatype)(end-1).timestamp
+%             fprintf('GAP in the data - sensor: %s datatype: %s \n', sensor, datatype)
+            package_gap_counter = package_gap_counter+1;
+        elseif cell.timestamp == all_data.(sensor).(datatype)(end-1).timestamp
+            vars = fieldnames(cell);
+            p = find(vars == "address");
+            vars(p:end) = [];
+            
+            data1 = zeros(length(vars),length(cell.timestamp));
+            data2 = zeros(length(vars),length(cell.timestamp));
+            for v = 1:length(vars)
+                data1(v,:) = cell.(vars{v});
+                data2(v,:) = all_data.(sensor).(datatype)(end-1).(vars{v});
+            end
+            if isequal(data1, data2)
+                all_data.(sensor).(datatype)(end) = [];
+                duplicate_data_counter = duplicate_data_counter+1;
+            else
+%                 fprintf('ERROR: different data for same time points - sensor: %s datatype: %s \n', sensor, datatype)
+                all_data.(sensor).(datatype)(end) = [];
+                duplicate_data_counter = duplicate_data_counter+1;
+            end
+        end
+               
+    end
+end
+
+
+%% convert data to nldat
+sensor_list = fieldnames(all_data);
+
+for n = 1:length(sensor_list)
+    
+    sensor = sensor_list{n};
+    data_list = fieldnames(all_data.(sensor));
+    for d = 1:length(data_list)
+        datatype = data_list{d};
+        y = all_data.(sensor).(datatype);
+        pkg_length = length(y);
+        
+        vars = fieldnames(all_data.(sensor).(datatype));
+        a = find(vars=="address");
+        vars(a:end) = [];
+
+        for v = 1:length(vars)
+            var = vars{v};
+            
+            data_length = length(all_data.(sensor).(datatype)(1).(var));
+            hold_data = zeros(data_length, pkg_length);
+            hold_time = zeros(data_length,pkg_length);
+            data={all_data.(sensor).(datatype).(var)};
+            time={all_data.(sensor).(datatype).timestamp};
+            for t = 1:pkg_length
+                    hold_data(:,t)=cell2mat(data(1,t));
+                    hold_time(:,t)=cell2mat(time(1,t));
+            end
+            % best time to test for gaps??
+            
+            hold_data=transpose(reshape(hold_data,1,[]));
+            hold_time=transpose(reshape(hold_time,1,[]));
+            hold_time=hold_time-hold_time(1,1);
+            
+            hold_nldat = nldat(hold_data);
+            set(hold_nldat, 'DomainValues', hold_time,'DomainName', "Time (ms)", 'ChanNames', string(var), 'comment', [sensor ' ' datatype])
+
+            if v > 1
+                eval(['nldat_' sensor '_' datatype '=cat(2,hold_nldat,  nldat_' sensor '_' datatype ');'])
+                
+            else
+                eval ([ 'nldat_' sensor '_' datatype '= hold_nldat;']);
+            end
+        end
+        if exist(['nldat_' sensor '_' datatype], 'var')
+            figure()
+            eval(['plot(nldat_' sensor '_' datatype ')'])
+            %savefig([savepath, 'trial' num2str(ntrial), '_', descrip_path '_' datatype sensor])
+        end
+    end
+end
+
+    
+
+toc/60
