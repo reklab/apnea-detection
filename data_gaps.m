0dacfa1576cfe76e0c1f5736fe4abbcef44279de
jasmine-10 <jasmine.tam@mail.mcgill.ca>
2021-11-02 12:39:17 -0400

    Update data_gaps.m

--- data_gaps.m
+++ data_gaps.m
@@ -4,10 +4,10 @@ time_C3892 = get(nldat_C3892_ACCEL, "domainValues");
 
 %intervals between subsequent data points
 time_C3898 = sort(unique(time_C3898));
-interval_C3898=diff(time_C3898);
+interval_C3898=round(diff(time_C3898),5);
 
 time_C3892 = sort(unique(time_C3892));
-interval_C3892=diff(time_C3892);
+interval_C3892=round(diff(time_C3892),5);
 
 % time_L3572 = sort(unique(time_L3572));
 % interval_L3572=diff(time_L3572);
@@ -19,21 +19,12 @@ gaps_C3892=interval_C3892(abs(interval_C3892-mode(interval_C3892))>0.02*mode(int
 
 figure()
 hist(gaps_C3898)
-title('Gaps for ACCEL sensor C3898');
-fprintf('Normal gap for ACCEL sensor C3898')
-% tabulate(gaps_C3898)
-mode(interval_C3898)
+title(sprintf('Gaps for ACCEL sensor C3898 (normal gap size = %s)', num2str(mode(interval_C3898))))
 
 figure()
 hist(gaps_C3892)
-title('Gaps for ACCEL sensor C3892');
-fprintf('Normal gap for ACCEL sensor C3892')
-% tabulate(gaps_C3892)
-mode(interval_C3892)
+title(sprintf('Gaps for ACCEL sensor C3892 (normal gap size = %s)', num2str(mode(interval_C3892))))
 
 % figure()
 % hist(gaps_L3572)
 % title('Gaps for temperature sensor L3572');
-% fprintf('Normal gap for temp sensor L3572')
-% % tabulate(gaps_C3892)
-% mode(interval_L3572)
