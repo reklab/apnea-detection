7edded6c5389faf0b9a7360447c21685cc82f645
Victoria Sturgess <victoria.sturgess@mail.mcgill.ca>
2021-11-08 19:20:54 -0500

    Fixed NaN in last segment

--- data_preprocess.m
+++ data_preprocess.m
@@ -1,8 +1,9 @@
 function [nldat1]=data_preprocess(nldat1, fs1, fs2, time, saveFigs)
 
+%nldat1=nldat_C3892_ACCEL;
 %fs2=500;
 nldat1 = interp1(nldat1, time, 'linear');
-
+%%
 nChans=3;
 L=length(nldat1.dataSet);
 WindowJump=5*fs2;
@@ -11,20 +12,27 @@ data=zeros(L,nChans,N);
 
 T1=1;
 T2=fs2*20;
-
+%%
 for n=1:N-4
     data(T1:T2,:,n)=detrend(nldat1(T1:T2,:),3);
     T1=T1+WindowJump;
     T2=T2+WindowJump;
+    if n>30
+    disp(n)
+    disp(T2)
+    end
 end
-
+T_stop=T2-WindowJump;
+%%
 detrended=zeros(L,nChans);
 for j=1:nChans
 for i=1:L
     detrended(i,j)=mean(nonzeros(data(i,j,:)));
 end
 end
-
+%%
+detrended=detrended(1:T_stop,:);
+%%
 nldat1.dataSet=detrended;
 %nldat=nldat(1:T1,:);
 data_1= get(nldat1, "dataSet");
