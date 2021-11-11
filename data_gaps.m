9155b9e399c79a73acc5d529322c8f570ea6cd47
jasmine-10 <jasmine.tam@mail.mcgill.ca>
2021-11-11 12:32:11 -0500

    Update data_gaps.m
    fixed issue with sensor name

--- data_gaps.m
+++ data_gaps.m
@@ -4,14 +4,16 @@
 function [gaps, interval]=data_gaps(nldat, savefigs, savepath)
 
 time = get(nldat, "domainValues");
-name = get(nldat, "chanNames");
-if sum(size(name))~=2
-    name = 'ACCEL';
-else
-    name = char(name{1});
-end
+data_type = get(nldat, "chanNames");
+t=nldat.comment{1};
 
-t=nldat.comment{1}; sensor_name=t(1:5);
+if sum(size(data_type))~=2
+    data_type = 'ACCEL';
+    sensor_name=t(12:16);
+else
+    data_type = char(data_type{1});
+    sensor_name=t(1:5);
+end
 
 %intervals between subsequent data points
 time = sort(unique(time));
@@ -23,13 +25,13 @@ gaps=interval(abs(interval-mode(interval))>0.02*mode(interval));
 if savefigs
     figure()
     hist(gaps,20)
-    title(sprintf('Gaps for %s sensor %s (normal gap size = %s)', name, sensor_name, num2str(mode(interval))))
-    savefig([savepath, 'gap_hist_' name '_' sensor_name])
+    title(sprintf('Gaps for %s sensor %s (normal gap size = %s)', data_type, sensor_name, num2str(mode(interval))))
+    savefig([savepath, 'gap_hist_' data_type '_' sensor_name])
     close all 
 end
 
 W = tabulate(interval);
 T  = array2table(W);
-save([savepath, 'tabulated_gaps_' name '_' sensor_name], 'T')
+save([savepath, 'tabulated_gaps_' data_type '_' sensor_name], 'T')
 
 end
