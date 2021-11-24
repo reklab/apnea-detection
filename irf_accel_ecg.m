53c200b11a184645806055cd7050d36257452d24
laura.carlton@mail.mcgill.ca <laura.carlton@mail.mcgill.ca>
2021-11-24 11:40:11 -0500

    updates_irf

--- irf_accel_ecg.m
+++ irf_accel_ecg.m
@@ -1,56 +1,48 @@
 
 % rough code outline to generate the impulse response
-clc
+
+function irf_accel_ecg(nldat_ACCEL_ouput, nldat_ECG)
+
 ts = 0.002;
-nldat_ACCEL_output = nldat_C3898_ACCEL(:,3);
-%nldat_ACCEL_output=seg_nldat_C3898.seg1 (:,3);
+% nldat_ACCEL_output = nldat_C3898_ACCEL(:,3);
+nldat_ACCEL_output = seg_C3898_ACCEL.seg2(:,3);
+set(nldat_ACCEL_output, 'domainIncr', ts, 'domainValues', NaN);
+
+ECG  = seg_ECG_C3898.seg2;
+set(ECG, 'domainIncr', ts, 'domainValues', NaN);
+
 x = nldat_ACCEL_output.dataSet;
 time_seg = 0:ts:length(x)*ts-ts;
 
-neg_nldat_C3898_ECG=nldat_C3898_ECG;
-neg_nldat_C3898_ECG.dataSet=-neg_nldat_C3898_ECG.dataSet;
-nldat_ECG_input = interp1(nldat_C3898_ECG, time_seg, 'linear');
+% nldat_ECG_input = interp1(nldat_C3898_ECG, time_seg, 'linear');
+nldat_ECG_input = interp1(ECG, time_seg, 'linear');
+set(nldat_ECG_input, 'domainIncr', ts, 'domainValues', NaN);
+
 nldat_sys = cat(2,nldat_ECG_input, nldat_ACCEL_output);
-nldat_sys = cat(2,nldat_ECG_input(1:7500,:), nldat_ACCEL_output(1:7500,:));
-% nldat_sys = cat(2,nldat_ACCEL_output, nldat_ECG_input);
+plot(nldat_sys)
 
-I = irf(nldat_sys);
+I = irf(nldat_sys, 'nLags', 60);
+figure()
+plot(I)
 
-ECG_data=nldat_ECG_input.dataSet;
-pred = conv(ECG_data(1:7500),I.dataSet);
-%pred = conv(nldat_ECG_input.dataSet,I.dataSet);
+regr = conv(nldat_ECG_input.dataSet,I.dataSet);
 
 %%
-% time_ypred = 0:ts:length(pred)*ts-ts;
-% plot(time_ypred, ypred, 'r')
+time_regr = 0:ts:length(regr)*ts-ts;
+% plot(time_ypred, regr, 'r')
 % hold on
-% plot(nldat_ECG_interp)
+% plot(nldat_ECG_input)
 
-%pred = pred(1:length(time_seg));
-pred = pred(1:7500);
-ACCEL_data=nldat_ACCEL_output(1:7500).dataSet;
-%clean = nldat_ACCEL_output.dataSet - pred;
-clean=ACCEL_data-pred;
+regr = regr(1:length(time_seg));
+clean = nldat_ACCEL_output.dataSet - regr;
 
 nldat_ACCEL_clean = nldat(clean, 'domainIncr', ts, 'domainValues', NaN);
-set(nldat_ACCEL_output, 'domainIncr', ts, 'domainValues', NaN);
+
 %%
-figure(2)
-hold on 
-plot(nldat_ACCEL_output(1:7500))
 plot(nldat_ACCEL_clean)
-legend(["input", "cleaned"])
+hold on
+plot(nldat_ACCEL_output)
+legend(["cleaned", "input"])
 hold off
 
-figure(3)
-plot(nldat_ACCEL_clean)
-
-
-figure(4)
-plot(nldat_ACCEL_output(1:7500))
-
-figure (5)
-plot(pred)
-
-%%
-nldat_ACCEL_output=nldat_ACCEL_output(1:7500);
+end
