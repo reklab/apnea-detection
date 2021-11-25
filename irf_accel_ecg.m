11bbd654f0159afb00dacb1133dea02777c923dd
Victoria Sturgess <victoria.sturgess@mail.mcgill.ca>
2021-11-25 17:41:47 -0500

    Update irf_accel_ecg.m

--- irf_accel_ecg.m
+++ irf_accel_ecg.m
@@ -3,7 +3,8 @@
 clc
 ts = 0.002;
 nldat_ACCEL_output = nldat_C3898_ACCEL(:,3);
-%nldat_ACCEL_output=seg_nldat_C3898.seg1 (:,3);
+% nldat_ACCEL_output=seg_nldat_C3898.seg1 (:,3);
+L=length(nldat_ACCEL_output)
 x = nldat_ACCEL_output.dataSet;
 time_seg = 0:ts:length(x)*ts-ts;
 
@@ -11,46 +12,35 @@ neg_nldat_C3898_ECG=nldat_C3898_ECG;
 neg_nldat_C3898_ECG.dataSet=-neg_nldat_C3898_ECG.dataSet;
 nldat_ECG_input = interp1(nldat_C3898_ECG, time_seg, 'linear');
 nldat_sys = cat(2,nldat_ECG_input, nldat_ACCEL_output);
-nldat_sys = cat(2,nldat_ECG_input(1:7500,:), nldat_ACCEL_output(1:7500,:));
-% nldat_sys = cat(2,nldat_ACCEL_output, nldat_ECG_input);
 
 I = irf(nldat_sys);
 
 ECG_data=nldat_ECG_input.dataSet;
-pred = conv(ECG_data(1:7500),I.dataSet);
-%pred = conv(nldat_ECG_input.dataSet,I.dataSet);
+
+pred = nlsim(I,nldat_ECG_input);
 
 %%
-% time_ypred = 0:ts:length(pred)*ts-ts;
-% plot(time_ypred, ypred, 'r')
-% hold on
-% plot(nldat_ECG_interp)
-
-%pred = pred(1:length(time_seg));
-pred = pred(1:7500);
-ACCEL_data=nldat_ACCEL_output(1:7500).dataSet;
-%clean = nldat_ACCEL_output.dataSet - pred;
-clean=ACCEL_data-pred;
-
+% cleaning ACCEL signal
+pred = pred(1:length(time_seg));
+clean = nldat_ACCEL_output.dataSet - pred.dataSet;
 nldat_ACCEL_clean = nldat(clean, 'domainIncr', ts, 'domainValues', NaN);
 set(nldat_ACCEL_output, 'domainIncr', ts, 'domainValues', NaN);
 %%
+% plotting results
 figure(2)
 hold on 
-plot(nldat_ACCEL_output(1:7500))
 plot(nldat_ACCEL_clean)
-legend(["input", "cleaned"])
+plot(nldat_ACCEL_output)
+% plot(pred)
+legend(["cleaned", "input", "pred"])
 hold off
 
-figure(3)
-plot(nldat_ACCEL_clean)
+% figure(3)
+% plot(nldat_ACCEL_clean)
+% 
+% figure(4)
+% plot(nldat_ACCEL_output(1:7500))
+% 
+% figure (5)
+% plot(pred)
 
-
-figure(4)
-plot(nldat_ACCEL_output(1:7500))
-
-figure (5)
-plot(pred)
-
-%%
-nldat_ACCEL_output=nldat_ACCEL_output(1:7500);
