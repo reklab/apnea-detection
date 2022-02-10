44c6ad9ca5c17bbe8faf7532acd62f6dd0a21a1f
jasmine-10 <jasmine.tam@mail.mcgill.ca>
2022-02-09 20:07:31 -0500

    Update pca_analysis.m

--- pca_analysis.m
+++ pca_analysis.m
@@ -5,7 +5,7 @@ labels=table2array(T1(:,67));
 
 [coeff,score,latent]=pca(input);
 
-categories=fieldnames(stat);
+categories=fieldnames(T);
 categories=categories(1:66);
 
 groups=zeros(length(labels),1);
@@ -20,6 +20,12 @@ for i=1:length(labels)
 end
 
 % biplotG(coeff, score, 'Groups', groups, 'Varlabels', categories)
+% figure()
 % biplot(coeff(:,1:3),'Scores',score(:,1:3),'Varlabels',categories);
 % mapcaplot(input,labels);
 
+principal_metrics=zeros(10,6);
+
+for i=1:6
+    [~,principal_metrics(:,i)]=maxk(coeff(:,i),10);
+end
