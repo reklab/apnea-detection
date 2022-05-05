33b0fa651d576b448c1207c8ff5c59bd33d7e351
laura.carlton@mail.mcgill.ca <laura.carlton@mail.mcgill.ca>
2022-05-05 10:09:14 -0400

    file_rename

--- feature_boxplot.m
+++ feature_boxplot.m
@@ -1,5 +1,8 @@
+%% feature_boxplot_v2
+% function is called by mean_freq_pk_phasediff
+% generates a boxplot of the frequency and amplitude features 
 
-function feature_boxplot(mean_freqs, se_freqs, mean_pks, se_pks, mean_phasediff, se_phasediff, savepath, sensor)
+function feature_boxplot_v2(mean_freqs, se_freqs, mean_pks, se_pks,savepath, sensor)
 
 directions = ["X", "Y", "Z"];
 i=1;
@@ -9,14 +12,12 @@ for v = 1:length(directions)
 
     mean_freq.(dir) = mean_freqs([i,i+3,i+6]);
     mean_amp.(dir) = mean_pks([i,i+3,i+6]);
-    mean_phzdiff.(dir) = mean_phasediff([i,i+3,i+6]);
     
     se_freq.(dir) = se_freqs([i,i+3,i+6]);
     se_amp.(dir) = se_pks([i,i+3,i+6]);
-    se_phzdiff.(dir) = se_phasediff([i,i+3,i+6]);
     i = i+1;
 end
-%%
+%% individual boxplots for each each feature and each direction 
 x_labels = categorical(["normal", "voluntary", "obstructive"]);
 ftsz =14;
 datasz = 150;
@@ -26,81 +27,64 @@ i = 1;
 for v = 1:length(directions)
 
     dir = directions{v};
-    ax1 = subplot(3,3,i);
+    ax1 = subplot(3,2,i);
     scatter(x_labels, mean_freq.(dir), 'k', 'filled', 'marker', 's', 'SizeData', datasz)
     hold on
     errorbar(x_labels,mean_freq.(dir), se_freq.(dir), 'k', 'Marker', 'none', 'LineStyle', 'none');
-    title ([ sensor ': Peak frequency in direction ' dir]);
-    % xlabel ('Breathing Type and Direction');
+    if i ==1
+        title ([ sensor ': Peak frequency in direction ' dir]);
+    else 
+        title ([ 'Peak frequency in direction ' dir]);
+    end
     ylabel ('Mean Across trials');
-    % ylim([0,3])
     set(ax1, 'FontSize',ftsz, 'XGrid', 'on', 'YGrid', 'on', 'GridLineStyle', '--', 'FontWeight', 'bold');
-    if i>6
+    if i>=4
             xlabel ('Breathing Type');
     end
 
-    ax2 = subplot(3,3,i+1);
+    ax2 = subplot(3,2,i+1);
     scatter(x_labels, mean_amp.(dir), 'k', 'filled', 'marker', 's', 'SizeData', datasz)
     hold on
     errorbar(x_labels, mean_amp.(dir), se_amp.(dir), 'k', 'Marker', 'none', 'LineStyle', 'none');
-    title (['Peak amplitude in direction ' dir]);
-    % xlabel ('Breathing Type and Direction');
+    if i == 1
+        title ([sensor ': Peak amplitude in direction ' dir]);
+    else
+        title (['Peak amplitude in direction ' dir]);
+    end
     ylabel ('Mean Across trials');
     set(ax2, 'FontSize',ftsz, 'XGrid', 'on', 'YGrid', 'on', 'GridLineStyle', '--', 'FontWeight', 'bold');
-    if i>6
+    if i>=4
             xlabel ('Breathing Type');
     end
 
-    ax3 = subplot(3,3,i+2);
-    scatter(x_labels, mean_phzdiff.(dir), 'k', 'filled', 'marker', 's', 'SizeData', datasz)
-    hold on
-    errorbar(x_labels, mean_phzdiff.(dir), se_phzdiff.(dir), 'k', 'Marker', 'none', 'LineStyle', 'none');
-    title (['Phase difference at peak in direction ' dir]);
-    ylabel ('Mean Across trials');
-    set(ax3, 'FontSize',ftsz, 'XGrid', 'on', 'YGrid', 'on', 'GridLineStyle', '--', 'FontWeight', 'bold');
-    if i>6
-            xlabel ('Breathing Type');
-    end
-
-    i = i+3;
+    i = i+2;
 end
 
 savefig(gcf, [savepath, 'boxplots_meanfreq_' sensor])
 
-%% one big boxplot
+%% one boxplot with features compiled from all directions
 
 x_labels = categorical(["X normal", "X voluntary", "X obstructive", "Y normal", "Y voluntary", "Y obstructive", "Z normal", "Z voluntary", "Z obstructive"]);
 ftsz =16;
 datasz = 150;
 figure('position', [ 1         194        1440         800]);
 
-ax1 = subplot(311);
+ax1 = subplot(211);
 scatter(x_labels, mean_freqs, 'k', 'filled', 'marker', 's', 'SizeData', datasz)
 hold on
 errorbar(x_labels,mean_freqs, se_freqs, 'k', 'Marker', 'none', 'LineStyle', 'none');
 title ([sensor ': Peak frequency in direction']);
-% xlabel ('Breathing Type and Direction');
 ylabel ('Mean Across trials');
-% ylim([0,3])
 set(ax1, 'FontSize',ftsz, 'XGrid', 'on', 'YGrid', 'on', 'GridLineStyle', '--', 'FontWeight', 'bold');
 
-ax2 = subplot(312);
+ax2 = subplot(212);
 scatter(x_labels, mean_pks, 'k', 'filled', 'marker', 's', 'SizeData', datasz)
 hold on
 errorbar(x_labels, mean_pks, se_pks, 'k', 'Marker', 'none', 'LineStyle', 'none');
 title ('Peak amplitude in direction');
-% xlabel ('Breathing Type and Direction');
 ylabel ('Mean Across trials');
 set(ax2, 'FontSize',ftsz, 'XGrid', 'on', 'YGrid', 'on', 'GridLineStyle', '--', 'FontWeight', 'bold');
 
-ax3 = subplot(313);
-scatter(x_labels, mean_phasediff, 'k', 'filled', 'marker', 's', 'SizeData', datasz)
-hold on
-errorbar(x_labels, mean_phasediff, se_phasediff, 'k', 'Marker', 'none', 'LineStyle', 'none');
-title ('Phase difference at peak in direction');
-xlabel ('Breathing Type');
-ylabel ('Mean Across trials');
-set(ax3, 'FontSize',ftsz, 'XGrid', 'on', 'YGrid', 'on', 'GridLineStyle', '--', 'FontWeight', 'bold');
 
 savefig(gcf, [savepath, 'boxplot_meanfreq_' sensor '_singleplot'])
 
