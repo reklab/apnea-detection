e12acb4d18a59ebfe22a26897077260cd31ea96b
Victoria Sturgess <victoria.sturgess@mail.mcgill.ca>
2022-05-10 00:01:48 -0400

    Added comments for clarity
    Added comments and restructured code for clarity for new users

--- MultipleModels.m
+++ MultipleModels.m
@@ -1,37 +1,58 @@
-%Function to call train classifier with an increasing number of features
-% addpath('C:\Users\vstur\OneDrive\Desktop\BIEN 470 DATA\7 Feb')
-%Need Training Table, Testing Table, label_tables (contains metric name, imp, and metricNum)
-%TrainClassifier Function
-n=5;
-UseMetric=false(1,height(label_tables));
-%N=height(label_tables;
-Max=65;
-Min=10;
-inc=10;
+%Function to call train classifier to generate multiple models while varing
+%the value of a single feature
 
-% clear Models
-% 
-for i=1:1:height(label_tables)
+%Need Table_Train, Table_Test, label_tables (contains metric name, imp, and metricNum)
+baseDir1=strcat(['.../Dropbox/ApnexDetection_Project/trials_data_nldat_v3/ANNE_data_tables']);
+load(baseDir1)
+
+baseDir2=strcat(['.../Dropbox/ApnexDetection_Project/trials_data_nldat_v3/label_tables']);
+load(baseDir2)
+
+clear Models
+
+
+Max=5;
+Min=1;
+inc=1;
+
+
+%% If varying the number of metrics
+% n=5 %the value assigned to n means that the classifier is only called for multiples on n
+
+% UseMetric=false(1,height(label_tables));
+% N=height(label_tables);
+
+% for i=1:1:height(label_tables)
+%     UseMetric(label_tables.MetricNum(i))=true;
+%     if rem(i,n)==0
+%         L=sprintf('Metric%d', i)
+%         [holdTF, holdVA]=Boost_Trainer(Table_Train, 'RUSBoost', 100, 25, 0.1, UseMetric);
+%         Models.(L).TrainedClassifier=holdTF;
+%         Models.(L).ValidationAcc=holdVA;
+%     end
+% end
+
+%% If varying any other parameter
+N=height(label_tables);
+for i=1:1:N
     UseMetric(label_tables.MetricNum(i))=true;
-    if rem(i,n)==0
-    L=sprintf('Metric%d', i)
-    [holdTF, holdVA]=Bag_Trainer(Table_Train, 100, 25, UseMetric);
-    Models.(L).TrainedClassifier=holdTF;
-    Models.(L).ValidationAcc=holdVA;
-    end
 end
 
-% for i=1:1:40
-%     UseMetric(label_tables.MetricNum(i))=true;
-% end
-% 
-% 
-% for i=Min:inc:Max
-%     L=sprintf('Metric%d', i)
-%     [holdTF, holdVA]=Bag_Trainer(Table_Train, i, 10, UseMetric);
-%     Models.(L).TrainedClassifier=holdTF;
-%     Models.(L).ValidationAcc=holdVA;
-% end
+%Can use either Boost_Trainer or Bag_Trainer
+for i=Min:inc:Max
+    L=sprintf('Metric%d', i)
+    % Use if varying number of splits
+%         [holdTF, holdVA]=Boost_Trainer(Table_Train,'AdaBoostM2', i, 25, 1, UseMetric);
+
+    % Use if varying number of trees
+%         [holdTF, holdVA]=Boost_Trainer(Table_Train,'AdaBoostM2', 50, i, 1, UseMetric);
+
+    % Use if varying learning rate (ony for Boost_Trainer
+%         j=[0.001, 0.01, 0.1, 0.5, 1];
+%         [holdTF, holdVA]=Boost_Trainer(Table_Train,'AdaBoostM2', 50, 25, j(i), UseMetric);
+    Models.(L).TrainedClassifier=holdTF;
+    Models.(L).ValidationAcc=holdVA;
+end
 %%
 clear CrossVal_Acc TestAcc Sensitivity Specificity WrongApnea
 CrossVal_Acc=zeros(1,Max);
@@ -40,8 +61,8 @@ ID_Test_Array=Table_Test.ID;
 Sensitivity=zeros(1,Max);
 Specificity=zeros(1,Max);
 CorrectApnea=zeros(1,Max);
-%for i=n:n:height(label_tables)
-for i=Min:inc:Max   
+%for i=n:n:height(label_tables) %use if varying number of metrics
+for i=Min:inc:Max               %use if varying any other parameter
     L=sprintf('Metric%d', i);
     CrossVal_Acc(i)=Models.(L).ValidationAcc;
     
@@ -74,29 +95,27 @@ TestAcc(TestAcc==0)=[];
 Sensitivity(Sensitivity==0)=[];
 Specificity(Specificity==0)=[];
 CorrectApnea(CorrectApnea==0)=[];
-%% Assume all data labeled as N
-N_string=ID_Test_Array;
-N_string(1:end)='N';
-C_N=confusionmat(ID_Test_Array,N_string);
-Right=C_N(1,1)+C_N(2,2)+C_N(3,3);
-Acc=Right/height(Table_Test);
-N_Acc(Min:Max)=Acc;
+%% Plot Data
+
+%if varying learning rate for boost_Trainer
+%i=j;
+
 
 figure()
 hold on
-plot(X,TestAcc);
-plot(X,CrossVal_Acc);
+plot(i,TestAcc);
+plot(i,CrossVal_Acc);
 legend('Accuracy of Testing Group','Cross Validation Accuracy of Training Group', 'Accuracy if Model always predicts N')
-xlabel('Number of Trees')
+xlabel('Training Time')
 ylabel('Percent')
 title('Accuracy of Testing and Training Group')
 
 figure()
 hold on
-plot(X,Sensitivity);
-plot(X,Specificity);
-plot(X,CorrectApnea);
+plot(i,Sensitivity);
+plot(i,Specificity);
+plot(i,CorrectApnea);
 legend('Sensitivity','Specificity', 'Percent Correct Apnea')
-xlabel('Number of Trees')
+xlabel('Training Time')
 ylabel('Percent')
 title('Sensitivity and Specificity')
